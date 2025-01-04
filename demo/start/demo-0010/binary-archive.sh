dcff6eaf9f195f9d780dfdf6471fa2254ec4f388
samwhelp <samwhelp@users.noreply.github.com>
2025-01-04 19:47:52 +0800

    fix

--- demo/start/demo-0010/binary-archive.sh
+++ demo/start/demo-0010/binary-archive.sh
@@ -10,7 +10,7 @@ mod_binary_archive () {
 	local squashfs_file_name="${REF_SQUASHFS_FILE_NAME}"
 	local squashfs_dir_name="${REF_SQUASHFS_DIR_NAME}"
 
-	local new_squashfs_file_name="${squashfs_file_name}"
+	local new_squashfs_file_name="new.${squashfs_file_name}"
 
 
 	cd ./tmp
