99cb83247efcc28f5fcd1c88fe196f19a9927970
samwhelp <samwhelp@users.noreply.github.com>
2025-01-04 21:05:53 +0800

    fix

--- demo/start/demo-0010/config.sh
+++ demo/start/demo-0010/config.sh
@@ -1,10 +1,10 @@
 
-REF_ISO_FILE_NAME="ubuntu-24.04.1-live-server-amd64.iso"
-REF_ISO_URL="https://releases.ubuntu.com/24.04/${REF_ISO_FILE_NAME}"
+REF_ISO_FILE_NAME="ubuntu-22.04.5-live-server-amd64.iso"
+REF_ISO_URL="https://releases.ubuntu.com/22.04/${REF_ISO_FILE_NAME}"
 
-#REF_SQUASHFS_FILE_NAME="filesystem.squashfs"
+REF_SQUASHFS_FILE_NAME="filesystem.squashfs"
 #REF_SQUASHFS_FILE_NAME="ubuntu-server-minimal.squashfs"
-REF_SQUASHFS_FILE_NAME="ubuntu-server-minimal.ubuntu-server.squashfs"
+#REF_SQUASHFS_FILE_NAME="ubuntu-server-minimal.ubuntu-server.squashfs"
 
 REF_SQUASHFS_DIR_NAME="squashfs-root"
 
