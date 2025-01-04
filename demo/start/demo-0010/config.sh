c7c565894f953b8a131acd67a8b76b7307d4267a
samwhelp <samwhelp@users.noreply.github.com>
2025-01-04 18:51:19 +0800

    fix

--- demo/start/demo-0010/config.sh
+++ demo/start/demo-0010/config.sh
@@ -1,6 +1,6 @@
 
-REF_ISO_FILE_NAME="ubuntu-20.04.4-live-server-amd64.iso"
-REF_ISO_URL="https://releases.ubuntu.com/20.04.4/${REF_ISO_FILE_NAME}"
+REF_ISO_FILE_NAME="ubuntu-24.04.1-live-server-amd64.iso"
+REF_ISO_URL="https://releases.ubuntu.com/24.04/${REF_ISO_FILE_NAME}"
 
 
 REF_PACKAGE_LIST_INSTALL="xorriso squashfs-tools fakeroot"
